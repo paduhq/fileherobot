120dc99f73c5ea0517920b68900371bfde31cdec
paduhq <63902328+paduhq@users.noreply.github.com>
2020-05-28 01:15:01 +0800

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 import os
 class config():
-	TG_BOT_TOKEN=os.environ['1266425556:AAFUrB74aOU6sZHF7c4HRfqQcbAXFBNgAN4']
-	API_ID=os.environ['1250190']
-	API_HASH=os.environ['88a4e28966222536436bb4ab18e96d55']
+	TG_BOT_TOKEN=os.environ['TG_BOT_TOKEN']
+	API_ID=os.environ['API_ID']
+	API_HASH=os.environ['API_HASH']
